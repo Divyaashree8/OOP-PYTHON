c8d282a3b18ce63bf8ef4fd4d51669f560b0b000
Divyaashree8 <divyaashree8@gmail.com>
2026-04-13 17:40:16 +0530

    Refactor Student class constructor and add display method

---
+++ opp-py.py
@@ -0,0 +1,11 @@
+import json
+import os
+class Student:
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
+  def display(self):
+    print("Name:",self.name)
+    print("Age:", self.age)
+s1=Student("ABCD",18)
+s1.display()
