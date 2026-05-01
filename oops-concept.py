19d21f2dc97c8ba619a4ad7f63e319e71b7ffa60
Divyaashree8 <divyaashree8@gmail.com>
2026-05-01 18:58:00 +0530

    Refactor code to replace Student class with BankAccount
    Removed the Student class and its methods, and added a BankAccount class with methods to display account details.

--- oops-concept.py
+++ oops-concept.py
@@ -1,32 +1,16 @@
-import json
-import os
-class Student:
-  def __init__(self,id,name,age):
-    self.id=id
-    self.name=name
-    self.age=age
-def set_name(self,name):
-  if name.isalpha():
-    self.__name==name
-  else:
-    print("invalid name! only letters allowed")
- def set_name(self,age):
-  if age > 0 age <= 120:
-    self.__age==age
-  else:
-    print("invalid age! must between  and 120")
-    def get_name(self):
-      return self.name
-class StudentManager:
-  def __init__(self):
-    self.students=[]
-  def add_student(self,student):
-    self.students.append(student)
-  def display(self):
-    for s in self.students:
-    print("ID:"s.id,"Name:",s.name,"Age:", s.age)
-   
-s1=Student(1,"ABCD",18)
-s1.display()
-s1.set_name("AB")
-print(s1.get_name())
+
+class BankAccount:
+  def __init__(self,account_holder,account_number,balance):
+    self.account_holder=account_holder
+    self.account_number=account_number
+    self.balance=balance
+  def display_details(self):
+     print("Account Details)
+     print(f"Account Holder:{self.account_holder}")
+     print(f"Account Number:{self.account_number}")
+     print(f"Balance:${self.balance}")
+acc1=BankAccount("Divya"12345,500000)
+acc2=BankAccount("Shree",67895,500000)
+acc1.display_details()
+acc2.display_details()
+  
