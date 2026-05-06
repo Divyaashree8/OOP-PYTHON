7837b4b357416a885770a09eecb93049c011a8f5
Divyaashree8 <divyaashree8@gmail.com>
2026-05-06 17:09:31 +0530

    Implemented inheritance with savings account and current account classes
    Added display_details method and implemented SavingsAccount and CurrentAccount classes with specific withdraw logic.

--- oops-concept.py
+++ oops-concept.py
@@ -11,21 +11,38 @@ class BankAccount:
         print(f"${amount} deposited successfully.")
      else:
        print("Invaild amount! Deposite must be positive.")
-  def withdraw(self,amount):
-    if amount <=0:
-      print("Invalid amount! withdrawal must be positive")
-    elif amount> self.__balance:
-      print("Insufficient balance")  
-    else:
-      self.__balance-=amount
-      print(f"${amount}withdrawn successfully.")
+  
   def display_details(self):
      print("Account Details)
      print(f"Account Holder:{self.account_holder}")
      print(f"Account Number:{self.account_number}")
      print(f"Balance:${self.__balance}")
-acc1=BankAccount("Divya",12345,500000)
-acc2=BankAccount("Shree",67895,500000)
+  # child class
+  class SavingsAccount(Account):
+    MIN_Balance=1000
+    def withdraw(self,amount):
+    if amount <=0:
+      print("Invalid amount! withdrawal must be positive")
+    elif self.__balance - amount<self.MIM_Balance:
+      print("Cannot withdraw minimum balance must be maintained.")
+    elif amount> self.__balance:
+      print("Insufficient balance")  
+    else:
+      self.__balance-=amount
+      print(f"${amount}withdrawn successfully.")
+# child class 2
+class CurrentAccount(Account):
+    Overdraft_limit=5000
+    def withdraw(self,amount):
+    if amount <=0:
+      print("Invalid amount! withdrawal must be positive")
+    elif amount> self.__balance + self.Overdraft_limit:
+      print("Overdraft limit exceeded!")  
+    else:
+      self.__balance-=amount
+      print(f"${amount}withdrawn successfully.")
+acc1=SavingsAccount("Divya",12345,500000)
+acc2=CurrentAccount("Shree",67895,500000)
 acc1.display_details()
 acc2.display_details()
 acc1.deposite(2000)
